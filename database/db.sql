5016d354bbec9bec6a4215934db1ac10eeb359f7
LMerino420 <merinolev96@gmail.com>
2023-01-13 12:56:26 -0600

    Create db.sql

---
+++ database/db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE games_db;
+USE games_db;
+
+CREATE TABLE games(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tittle VARCHAR(180),
+    description VARCHAR(255),
+    image VARCHAR(200),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE games;
