e71b804bbb54d6523c4af920590a8976099132a9
LMerino420 <merinolev96@gmail.com>
2023-01-16 11:33:27 -0600

    Update Game

--- client/src/app/components/game-form/game-form.component.ts
+++ client/src/app/components/game-form/game-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, HostBinding } from '@angular/core';
+import { OnInit, Component, HostBinding } from '@angular/core';
 import { Game } from '../../models/game';
+import { Router, ActivatedRoute } from '@angular/router';
+
 import { GamesService } from '../../services/games.service';
 
 @Component({
@@ -7,7 +9,7 @@ import { GamesService } from '../../services/games.service';
   templateUrl: './game-form.component.html',
   styleUrls: ['./game-form.component.scss'],
 })
-export class GameFormComponent {
+export class GameFormComponent implements OnInit {
   @HostBinding('class') clases = 'row';
 
   game: Game = {
@@ -18,19 +20,60 @@ export class GameFormComponent {
     created_at: new Date(),
   };
 
-  constructor(private gameService: GamesService) {}
+  edit: boolean = false;
 
-  saveGame() {
+  constructor(
+    private gameService: GamesService,
+    private route: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  async ngOnInit() {
+    const params = this.activatedRoute.snapshot.params;
+    if (params['id']) {
+      const data = await this.gameService.getOneGame(params['id']);
+      data.subscribe(async (res: any) => {
+        const code = res.code;
+        const obj = res.object;
+        if (code === 'SUCCESS') {
+          console.log(obj);
+          this.game = obj;
+          this.edit = true;
+        } else {
+          console.log('NOT_FOUND');
+        }
+      });
+    }
+  }
+
+  async saveGame() {
     delete this.game.id;
     delete this.game.created_at;
-    const data = this.gameService.saveGame(this.game);
+    const data = await this.gameService.saveGame(this.game);
     data.subscribe(async (res: any) => {
       const code = res.code;
-      if (code == 'SUCCESS') {
+      if (code === 'SUCCESS') {
         console.log('JUEGO GUARDADO =>', this.game.tittle);
+        this.route.navigate(['/games']);
       } else {
         console.log('NO SE PUDO GUARDAR');
       }
     });
+  }
+
+  async updateGame() {
+    const id = String(this.game.id);
+    delete this.game.created_at;
+    const data = await this.gameService.updateGame(id, this.game);
+    data.subscribe(async (res: any) => {
+      const code = res.code;
+      if (code === 'SUCCESS') {
+        console.log('SE ACTUALIZO');
+        this.route.navigate(['/games']);
+      } else {
+        console.log('NO SE PUDO ACTUALIZAR');
+      }
+    });
+    console.log('UPDATE', this.game);
   }
 }
